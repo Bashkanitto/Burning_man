e10006a2757e860843c5516733879c0ee79fb7e2
Bashkanitto <86559848+Bashkanitto@users.noreply.github.com>
2022-05-30 09:37:45 +0300

    burning_man

---
+++ script.js
@@ -0,0 +1,21 @@
+// let a = document.querySelector('.content')
+// let all = document.getElementsByClassName('*')
+// let question = prompt('Enter password')
+// while(question!=1574){
+
+
+    
+//     let qu = prompt('Enter password')
+//     if (qu == 1574){
+//         question = 1574;
+//     }
+
+
+
+
+//     setTimeout(function(){
+
+//         location.reload();
+    
+//     }, 1000);
+// }
